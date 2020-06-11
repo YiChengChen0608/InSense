562048ca134867d4a39f43f23a2a9e7f6d695b2f
YiCheng <dekknm0971@gmail.com>
2020-06-11 12:35:26 +0800

    add card component

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ import '../styles/nav.scss'
 const Nav = () => {
   return (
     <>
-      <nav className='nav'>
+      <nav className='nav d-flex justify-content-between align-items-center'>
         <div className='leftItem'>
           <a href='javascript:;' role='button'><FiMenu /></a>
           <a href='javascript:;' role='button'><FiSearch /></a>
