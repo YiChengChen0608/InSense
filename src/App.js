a00a5059797edf032ee896f1a77fd154a043ebb6
YiCheng <dekknm0971@gmail.com>
2020-06-11 16:40:11 +0800

    add classList page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import 'normalize.css'
 import Nav from './components/nav'
-import ClassHead from './components/class/classHead'
 import Footer from './components/footer'
 
 
