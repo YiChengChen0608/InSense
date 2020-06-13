955c35641359cbb78226e8c1d085ab42f12b851e
YiCheng <dekknm0971@gmail.com>
2020-06-13 20:51:39 +0800

    RWD page detail

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import '../styles/style.scss'
 
 const MainContent = (props) => {
   return (
-    <div className='contain'>
-      {props.children}
+    <div className='main-container d-flex'>
+      <div className='contain'>
+        {props.children}
+      </div>
     </div>
   )
 }
