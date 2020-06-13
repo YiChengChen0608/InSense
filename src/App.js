a8784c085b13a826c8ff6387e56bcb7ccf9f06f2
peggy <ya5471ya@gmail.com>
2020-06-13 13:49:46 +0800

    before pull 6/13

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import 'normalize.css'
 import Nav from './components/nav'
+import Footer from './components/footer'
 import ClassCard from './components/class/classCard'
 import classImg from './images/class1.jpg'
-import Footer from './components/footer'
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
     <>
       <Nav />
       <ClassCard src={classImg} date={'2020/12/31'} className={'LFP 客製化香水課程預約'} />
+      
       <Footer />
     </>
   );
