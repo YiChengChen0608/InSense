66f82add326f3a5f03deb15a5a6ab1f5736bef69
YiCheng <dekknm0971@gmail.com>
2020-06-17 12:25:02 +0800

    add nav scroll animate

---
+++ src/components/IndexShoppingCart/indexShoppingCart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './indexShoppingCart.scss'
+
+const IndexShoppingCart = () => {
+  return (
+    <>
+      <p className='index-shopping-cart-title'>購物車</p>
+      <div className='index-shopping-cart-container'></div>
+      <a role='button'></a>
+    </>
+  )
+}
+
+export default IndexShoppingCart
