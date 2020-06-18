ff3747a80a593117e2639e726a1e7ae622f453d8
YiCheng <dekknm0971@gmail.com>
2020-06-18 10:14:47 +0800

    add index shopping cart

--- src/components/IndexSubscribe/indexSubscribe.js
+++ src/components/IndexSubscribe/indexSubscribe.js
@@ -3,14 +3,16 @@ import "./indexSubscribe.scss";
 import { Link } from "react-router-dom";
 
 const indexSubscribe = () => {
-    return (
-        <div className="index-subscribe-container text-center">
-            <p>訂閱我們</p>
-            <input type="text" placeholder="your email address*" />
-            <Link className="button-link" role="button">
-                SUBSCRIBE
+  return (
+    <div className="index-subscribe-container text-center">
+      <p>訂閱我們</p>
+      <div>
+        <input type="text" placeholder="your email address*" />
+      </div>
+      <Link className="button-link" role="button">
+        SUBSCRIBE
             </Link>
-        </div>
-    );
+    </div>
+  );
 };
 export default indexSubscribe;
--- src/pages/ClassList/classList.js
+++ src/pages/ClassList/classList.js
@@ -4,38 +4,38 @@ import ClassCard from "../../components/ClassCard/classCard";
 import MainContainer from "../../components/mainContainer";
 import "./classList.scss";
 const ClassList = () => {
-    return (
-        <>
-            <ClassHead
-                src={"/images/banner/classBanner.jpeg"}
-                title={"課程體驗"}
-                content={`InSense提供客製化香水體驗課程`}
-                content2={"與您一起感受香氛"}
-            />
-            <MainContainer otherClass="class-list-container d-flex flex-wrap justify-content-center">
-                <ClassCard
-                    src={"/images/banner/class1.jpg"}
-                    date={"2020/12/31"}
-                    Name={"LFP 客製化香水課程預約"}
-                />
-                <ClassCard
-                    src={"/images/banner/class1.jpg"}
-                    date={"2020/12/31"}
-                    Name={"LFP 客製化香水課程預約"}
-                />
-                <ClassCard
-                    src={"/images/banner/class1.jpg"}
-                    date={"2020/12/31"}
-                    Name={"LFP 客製化香水課程預約"}
-                />
-                <ClassCard
-                    src={"/images/banner/class1.jpg"}
-                    date={"2020/12/31"}
-                    Name={"LFP 客製化香水課程預約"}
-                />
-            </MainContainer>
-        </>
-    );
+  return (
+    <>
+      <ClassHead
+        src={"/images/banner/classBanner.jpeg"}
+        title={"課程體驗"}
+        content={`InSense提供客製化香水體驗課程`}
+        content2={"與您一起感受香氛"}
+      />
+      <MainContainer otherClass="class-list-container d-flex flex-wrap justify-content-center">
+        <ClassCard
+          src={"/images/class/class1.jpg"}
+          date={"2020/12/31"}
+          Name={"LFP 客製化香水課程預約"}
+        />
+        <ClassCard
+          src={"/images/class/class1.jpg"}
+          date={"2020/12/31"}
+          Name={"LFP 客製化香水課程預約"}
+        />
+        <ClassCard
+          src={"/images/class/class1.jpg"}
+          date={"2020/12/31"}
+          Name={"LFP 客製化香水課程預約"}
+        />
+        <ClassCard
+          src={"/images/class/class1.jpg"}
+          date={"2020/12/31"}
+          Name={"LFP 客製化香水課程預約"}
+        />
+      </MainContainer>
+    </>
+  );
 };
 
 export default ClassList;
