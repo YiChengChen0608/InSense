0b5c00e0934e367c007875eca9ae2f8c8a5c0d0a
Ting <chungtingchou@gmail.com>
2020-06-19 18:32:43 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import 'normalize.css'
 import Nav from './components/nav'
 import Footer from './components/footer'
-import ClassDetail from './pages/class/classDetail'
+import CSContent from './components/class/csContent'
+// import ClassDetail from './pages/class/classDetail'
+
 function App() {
   return (
     <>
       <Nav />
-      <ClassDetail />
+      <CSContent />
       <Footer />
     </>
   );
