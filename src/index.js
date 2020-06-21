39ab9f94790be5f56e5366a5298a6e08796d5f4a
peggy <ya5471ya@gmail.com>
2020-06-21 13:13:29 +0800

    import redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'rect-router-dom'
+
 import App from "./App";
 // import App from './Test';
 import * as serviceWorker from "./serviceWorker";
 
+
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <BrowserRouter>
+  <App />
+ </BrowserRouter>,
   document.getElementById("root")
 );
 
