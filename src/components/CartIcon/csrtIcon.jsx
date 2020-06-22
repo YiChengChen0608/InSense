bb973c70d88da88126192b8ed7dedc9e809a759a
peggy <ya5471ya@gmail.com>
2020-06-22 10:41:33 +0800

    failed version

---
+++ src/components/CartIcon/csrtIcon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+
+// import { toggleCartHidden } from '../../redux/cart/cart.actions';
+// import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+import { ReactComponent as ShoppingIcon } from '../../../public/images/icon/shopping-cart-solid.svg';
+
+import './cartIcon.scss';
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+    <div className='cart-icon' onClick={toggleCartHidden}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{itemCount}</span>
+    </div>
+  );
+
+  export default CartIcon;
+  
--- src/components/Redux/store.js
+++ src/components/Redux/store.js
@@ -2,7 +2,7 @@
 
 //Add middleware, so we can catch acions when they get fired or dispatched and then dispatch them
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import logger from './redux-logger';
 import rootReducer from './rootReducer';
 
 const middlewares = [logger];
