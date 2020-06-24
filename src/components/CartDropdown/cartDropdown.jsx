bb979494d9c633833cec945259044c0a3984d679
YiCheng <dekknm0971@gmail.com>
2020-06-24 20:49:09 +0800

    update css

--- src/components/CartDropdown/cartDropdown.jsx
+++ src/components/CartDropdown/cartDropdown.jsx
@@ -4,10 +4,10 @@ import SubmitButton from '../SubmitButton/submitButton';
 import './cartDropdown.scss';
 
 const CartDropdwon = () => (
-    <div className="cart-dropdown">
-        <div className="cart-items" />
-        <SubmitButton>結帳</SubmitButton>
-    </div>
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <SubmitButton inverted={true}>結帳</SubmitButton>
+  </div>
 )
 
 export default CartDropdwon;
