47d5f93d2208675a1f7bfc4874aec0bf0cbcc547
YiCheng <dekknm0971@gmail.com>
2020-06-25 21:29:27 +0800

    my account reservation

--- src/components/classItem.js
+++ src/components/classItem.js
@@ -3,13 +3,15 @@ import '../components/Classes/classes'
 
 const ClassItem = (props) => {
   return (
-    <div className='d-flex class-item text-center'>
-      <p>{props.classInfo.classTime}</p>
-      <p>{props.classInfo.bookTime}</p>
-      <p>{props.classInfo.classCategoryName}</p>
-      <p>{props.classInfo.className}</p>
-      <p>NT$ {props.classInfo.classPrice}</p>
-      <p>{props.classInfo.bookQty}</p>
+    <div className='d-flex class-item text-center align-items-center'>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.classTime}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.bookTime}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.classCategoryName}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.className}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>NT$ {props.classInfo.classPrice}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.bookQty}</p>
+      <p className={props.cancelBtn ? 'cancelBtn' : 'noCancelBtn'}>{props.classInfo.bookStatus}</p>
+      {props.cancelBtn ? <a className='cancelBtn' onClick={props.cancel} role='button' data-bookid={props.classInfo.bookId}>取消</a> : ''}
     </div>
   )
 }
