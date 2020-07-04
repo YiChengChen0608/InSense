2fdd2e58933d296edf4cd07d5684f982edbd0725
瑞瑜 <ruiyu@ruiyude-MacBook-Pro.local>
2020-07-04 17:49:36 +0800

    0704

--- src/components/ItemImg/itemImg.js
+++ src/components/ItemImg/itemImg.js
@@ -1,30 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./itemImg.scss";
 
 const ItemImg = (props) => {
-  return (
-    <>
-      <div className="item-gallery-container">
-        <div className="item-gallery">
-          <div className="detail-img-big">
-            <img src={props.itemimg} />
-          </div>
-          <div className="d-flex">
-            <div className="detail-img-small">
-              <img src={props.itemimg1} />
+    const [pictureShown, setPictureShown] = useState(props.itemimg);
+
+    const newPictureShow = (e) => {
+        // console.log(e.currentTarget.dataset.picture);
+        setPictureShown(e.currentTarget.dataset.picture);
+    };
+    return (
+        <>
+            <div className="item-gallery-container">
+                <div className="item-gallery">
+                    <div className="detail-img-big">
+                        <img src={pictureShown} />
+                    </div>
+                    <div className="d-flex">
+                        <div
+                            className="detail-img-small"
+                            data-picture={props.itemimg1}
+                            onClick={newPictureShow}
+                        >
+                            <img src={props.itemimg1} />
+                        </div>
+                        {props.itemimg2 ? (
+                            <div
+                                className="detail-img-small img-second"
+                                data-picture={props.itemimg2}
+                                onClick={newPictureShow}
+                            >
+                                <img src={props.itemimg2} />
+                            </div>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                </div>
             </div>
-            {props.itemimg2 ? (
-              <div className="detail-img-small img-second">
-                <img src={props.itemimg2} />
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ItemImg;
