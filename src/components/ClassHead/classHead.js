f2a27e793f4fbe2e1dcace919145e6a47955a858
YiCheng <dekknm0971@gmail.com>
2020-07-05 00:43:23 +0800

    update class list and detail head

--- src/components/ClassHead/classHead.js
+++ src/components/ClassHead/classHead.js
@@ -4,14 +4,13 @@ import './classHead.scss'
 const ClassHead = ({ classImgDir, classImg, title, content, content2 }) => {
   return (
     <>
-      <div className="class-head d-flex flex-wrap align-items-center">
+      <div className="class-head">
         <div className="headLeft">
           <img className="object-fit-cover" src={`http://localhost:3030/images/${classImgDir}/${classImg}.png`} />
         </div>
-        <div className="headRight">
+        <div className="headRight ">
           <p className="headTitle">{title}</p>
-          <p className="headContent">{content}</p>
-          <p className="headContent">{content2}</p>
+          <p className="headContent">{content}{content2}</p>
         </div>
       </div>
     </>
